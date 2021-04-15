a85520992fdff0691b7e4992c509824d302da8c4
None <zanoni.reibnitz@gmail.com>
2021-04-15 20:53:34 -0300

    Chapter 3

---
+++ chapter3/process_pool.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+""" Chopping vegetables with a ThreadPool """
+
+import os
+from concurrent.futures import ProcessPoolExecutor
+
+
+def vegetable_chopper(vegetable_id):
+    name = os.getpid()
+    print(name, 'chopped a vegetable', vegetable_id)
+
+
+if __name__ == '__main__':
+    with ProcessPoolExecutor(max_workers=5) as pool:
+        for vegetable in range(100):
+            pool.submit(vegetable_chopper, vegetable)
