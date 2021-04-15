69ce6aef5a080cf064ff8093272b42d96210441a
None <zanoni.reibnitz@gmail.com>
2021-04-15 11:04:22 -0300

    Chapter 1

---
+++ README.md
@@ -0,0 +1,4 @@
+# Python Parallel and Concurrent Programming Part 2
+[Course](https://www.linkedin.com/learning/python-parallel-and-concurrent-programming-part-2?u=26137906)
+
+Contents divided by folders for each chapter.
---
+++ chapter1/semaphore.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3
+""" Connecting cell phones to a charger """
+
+import random
+import threading
+import time
+
+charger = threading.Semaphore(2)
+
+
+def cellphone():
+    name = threading.current_thread().getName()
+    with charger:
+        print(name, 'is charging...')
+        time.sleep(random.uniform(1, 2))
+        print(name, 'is DONE charging!')
+
+
+if __name__ == '__main__':
+    for phone in range(10):
+        threading.Thread(target=cellphone, name='Phone-' + str(phone+1)).start()
